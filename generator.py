b0b6ce6ca2ae337679858e2e32101c4a5fc79eed
komushinux <kom>
2022-10-23 00:47:17 +0300

    new logik

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
+import random
 
 
 def generate_linear(a, b, noise, filename, size=100):  # generate x,y for linear
@@ -117,89 +118,93 @@ def polynomial_regression_numpy(filename):
 # x and y are both vectors
 
 def gradient_descent_step(dJ, theta, alpha):
+    theta = theta - alpha * dJ
     return theta
 
 
 # get gradient over all xy dataset - gradient descent
 def get_dJ(x, y, theta):
-    alf = 0.001
-
-    # tmp_x = np.hstack([np.ones((100, 1)), x])
-    trp_theta = np.transpose(theta)
-    h = trp_theta.dot(x)
-    buf = h - y
-    for i in range(len(h)):
-        buf[i] *= x[i]
-    theta_new = theta
-    buf_sum = np.sum(buf)
-    for i in range(len(x)):
-        theta_new[i] = theta_new[i] - alf * buf_sum  # тета - альфа * частная производная
-    print('theta_new = ', theta_new)
-    return theta_new
+    x_trp = x.transpose()
+    y_trp = y.transpose()
+    h = np.dot(theta, x_trp)
+    dJ = 1 / len(y) * (h - y_trp).dot(x)
+    return dJ
 
 
 # get gradient over all minibatch of size M of xy dataset - minibatch gradient descent
 def get_dJ_minibatch(x, y, theta, M):
-    theta_new = theta
-    print("your code goes here - calculate new theta")
-    return theta_new
+    new_theta = np.ndarray
+    new_theta.reshape(np.shape(theta))
+    theta_trans = theta.transpose()
+    h = np.dot(theta_trans, x)  # calculate hypothesis
+    delta = 0
+    dj = [0] * len(theta)
+    while M + delta < len(x):
+        for i in range(len(theta)):  # calculate partial derivatives of J
+            for k in range(len(x)):
+                dj[i] += 1 / len(x) * (h[np.power(x, i)] - np.power(y, i)) * np.power(x[k], i)
+            # new_theta.itemset(i, gradient_descent_step(dj[i], theta[i], alpha))
+            delta = M + delta
+    return new_theta
 
 
 # get gradient over all minibatch of single sample from xy dataset - stochastic gradient descent
-def get_dJ_sgd(x, y, theta):
-    theta_new = theta
-    print("your code goes here - calculate new theta")
-    return theta_new
+def get_dJ_sgd(x, y, theta, alpha):
+    temp_x = x
+    temp_y = y
+    new_theta = np.ndarray
+    new_theta.reshape(np.shape(theta))
+    theta_trans = theta.transpose()
+    h = np.dot(theta_trans, x)  # calculate hypothesis
+    dj = [0] * len(theta)
+    for i in range(len(theta)):  # calculate partial derivatives of J
+        for k in range(len(x)):
+            dj[i] += 1 / len(x) * (h[np.power(x, i)] - np.power(y, i)) * np.power(x[k], i)
+    for i in range(len(x)):
+        index = random.randint(0, len(x))
+        new_theta.itemset(i, gradient_descent_step(dj[index], theta[index], alpha))
+    return new_theta
 
 
-# try each of gradient decent (complete, minibatch, sgd) for varius alphas
-# L - number of iterations
-# plot results as J(i)
-def minimize(filename, L):
-    with open(filename, 'r') as f:
-        data = np.loadtxt(f, delimiter=',')
-    x, y = np.hsplit(data, 2)
-    print("Shape of x and y are: ", np.shape(x), np.shape(y))
-
-    n = 2
-    theta_stud = np.zeros(n)  # you can try random initialization
-    dJ = np.zeros(n)
-
-    # theta_stud = np.polyfit(np.transpose(x)[0], np.transpose(y)[0], 1)
+def minimize(x, y, L):
+    alpha = 0.04
+    theta_count = np.zeros(2)
+    itr = [None] * L
+    J_mass = [None] * L
     for i in range(0, L):
-        theta_stud = get_dJ(x, y, theta_stud)  # here you should try different gradient descents
+        dJ = get_dJ(x, y, theta_count)  # here you should try different gradient descents
+        theta_count = theta_count - alpha * dJ
+        h = np.dot(theta_count, x.transpose())
+        J_mass[i] = 0.5 / len(y) * np.square(h - y.transpose()).sum(axis=1)
+        itr[i] = i
 
-    h = theta_stud[1] * x + theta_stud[0]
-    J = 1
-    print(np.shape(J))
     plt.title("Minimize task")
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.plot(theta_stud, J, "b.")
-    plt.legend()
+    plt.xlabel("iteration")
+    plt.ylabel("Loss func")
+    plt.plot(itr, J_mass, "b.")
     plt.show()
-    return theta_stud
+    return theta_count
 
 
 def shuffle(filename):
     with open(filename, 'r') as f:
-        data = np.loadtxt(f, delimiter=',')
-    x, y = np.hsplit(data, 2)
+        database = np.loadtxt(f, delimiter=',')
+    x, y = np.hsplit(database, 2)
 
-    x_stud = x[0:70]
-    y_stud = x[0:70]
-    data = np.hstack((x_stud, y_stud))
-    np.savetxt('stud.csv', data, delimiter=',')
+    x_stud = x[0:80]
+    y_stud = x[0:80]
+    database = np.hstack((x_stud, y_stud))
+    np.savetxt('stud.csv', database, delimiter=',')
 
-    x_test = x[70:85]
-    y_test = x[70:85]
-    data = np.hstack((x_test, y_test))
-    np.savetxt('test.csv', data, delimiter=',')
+    x_test = x[80:90]
+    y_test = x[80:90]
+    database = np.hstack((x_test, y_test))
+    np.savetxt('test.csv', database, delimiter=',')
 
-    x_valid = x[85:100]
-    y_valid = x[85:100]
-    data = np.hstack((x_valid, y_valid))
-    np.savetxt('valid.csv', data, delimiter=',')
+    x_valid = x[90:100]
+    y_valid = x[90:100]
+    database = np.hstack((x_valid, y_valid))
+    np.savetxt('valid.csv', database, delimiter=',')
 
 
 if __name__ == "__main__":
@@ -221,12 +226,22 @@ if __name__ == "__main__":
     # 1. shuffle data into train - test - valid
     shuffle('linear.csv')
     # 2. call minimize(...) and plot J(i)
-    theta = minimize('stud.csv', 10)
+    with open('stud.csv', 'r') as f:
+        data = np.loadtxt(f, delimiter=',')
+    x, y = np.hsplit(data, 2)
+    x = np.hstack([np.ones((80, 1)), x])
+    theta = minimize(x, y, 200)
     # 3. call check(theta1, theta2) to check results for optimal theta
-    check(theta[0], theta[1])
+    # modl = np.polyfit(np.transpose(x)[0], np.transpose(y)[0], 1)
+
+    theta1 = theta
+    theta1[0][0] = 0
+    theta1[0][1] = 0
+    print(theta)
+    print(f"Is model correct? - {check(theta, theta1)}")
     print("*" * 40)
 
-    # ex3. polinomial regression
+    # ex3. polynomial regression
     # 0. generate date with function generate_poly for degree=3, use size = 10, 20, 30, ... 100
     # for each size:
     # 1. shuffle data into train - test - valid
